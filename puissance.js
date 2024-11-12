11e8f191eccbb2235ed04ac00de66b1c78408646
Ambryal <gohieralexis@yahoo.fr>
2024-11-12 20:43:26 +0100

    Ajout des fichiers .js du dossier dist

--- puissance.js
+++ puissance.js
@@ -5530,7 +5530,7 @@ res.tx=div({0:btn,26:1,17:"..."})
 		
 		}
 	else{for(var b of rumble){if(!surpuissance || (surpuissance==1 && rota2.length<200) || (surpuissance==2 && rota2.length<42)){rota2.push([b])}}}
-	
+	cl(ENNEMY, ENNEMY_REVERSED)
 	if(ENNEMY) {rota2=[[await getBrute(ENNEMY)]];if(ENNEMY_REVERSED){cl("reversed");[rota1,rota2] = [rota2,rota1]}}
 	
 				simulFights({
